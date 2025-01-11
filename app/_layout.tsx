3903c13063d469d57677d41524f1e944035c8d34
Den Brivka <denbrivka@Dens-MacBook-Pro.local>
2025-01-11 12:16:54 +0200

    feat: tab for city

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,23 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import "react-native-reanimated";
+
+import { StyleSheet } from "react-native";
+
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+
+import { Header, Button } from "@rneui/themed";
+
+import { useColorScheme } from "@/hooks/useColorScheme";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -14,7 +25,7 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   useEffect(() => {
@@ -28,12 +39,37 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+        <Stack.Screen name="+not-found" />{" "}
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
   );
 }
+const styles = StyleSheet.create({
+  headerContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#397af8",
+    marginBottom: 20,
+    width: "100%",
+    paddingVertical: 15,
+  },
+  heading: {
+    color: "white",
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+  headerRight: {
+    display: "flex",
+    flexDirection: "row",
+    marginTop: 5,
+  },
+  subheaderText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
